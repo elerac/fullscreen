62b89b485e6dde7972dfa0078289f5088c406937
elerac <maenosuke079@gmail.com>
2020-09-21 01:21:07 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 import cv2
 from fullscreen import FullScreen
 
-win = FullScreen(0)
+screen = FullScreen(0)
 
 DELAY = 1200 #ms
 
@@ -10,32 +10,32 @@ image = cv2.imread(filename, 1)
 
 
 # Single int
-win.imshow(200)
+screen.imshow(200)
 cv2.waitKey(DELAY)
 
 # Single float
-win.imshow(0.2)
+screen.imshow(0.2)
 cv2.waitKey(DELAY)
 
 # RGB int tuple
-win.imshow((255, 100, 100))
+screen.imshow((255, 100, 100))
 cv2.waitKey(DELAY)
 
 # RGB flot tuple
-win.imshow((0.4, 1.0, 0.4))
+screen.imshow((0.4, 1.0, 0.4))
 cv2.waitKey(DELAY)
 
 # RGB int list
-win.imshow([100, 100, 255])
+screen.imshow([100, 100, 255])
 cv2.waitKey(DELAY)
 
 # RGB float list
-win.imshow([0.2, 0.8, 1.0])
+screen.imshow([0.2, 0.8, 1.0])
 cv2.waitKey(DELAY)
 
 # Image case
-win.imshow(image)
+screen.imshow(image)
 cv2.waitKey(DELAY)
 
 
-win.destroyWindow()
+screen.destroyWindow()
