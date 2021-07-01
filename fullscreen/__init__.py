23a8195fe9a4b3627b8f8bbc6c87981792df22e8
elerac <maenosuke079@gmail.com>
2021-07-01 23:33:19 +0900

    Update __init__.py

--- fullscreen/__init__.py
+++ fullscreen/__init__.py
@@ -7,5 +7,8 @@ def FullScreen(backend="tkinter", **kwargs):
     elif backend == "cv2":
         from . import cv2_fullscreen
         return cv2_fullscreen.FullScreen(**kwargs)
+    elif backend == "PyQt5":
+        from . import pyqt5_fullscreen
+        return pyqt5_fullscreen.FullScreen()
     else:
         return None
